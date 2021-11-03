4f5f459c27fa929d2f39a23e5ca8cc6dd788ced8
sarpio <kontakt@piotrsarnecki.com>
2021-11-03 11:59:47 +0100

    Initial commit

---
+++ access-service/src/test/java/com/sarpio/accessservice/AccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarpio.accessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
